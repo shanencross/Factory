fd978223c03cf0a26529ee6453ddfed0bf3dd8d8
Shanen Cross <shanencross@yahoo.com>
2021-06-04 07:13:02 -0700

    Fix dataset names to be plural

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,8 +4,8 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-    public virtual DbSet<Engineer> Engineer { get; set; }
-    public virtual DbSet<Machine> Machine { get; set; }
+    public virtual DbSet<Engineer> Engineers { get; set; }
+    public virtual DbSet<Machine> Machines { get; set; }
     public FactoryContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
