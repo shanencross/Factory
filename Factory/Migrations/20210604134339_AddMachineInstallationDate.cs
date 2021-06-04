8cfd2ce55615b72806599ba1f4a56fe77a7200eb
Shanen Cross <shanencross@yahoo.com>
2021-06-04 06:44:04 -0700

    Add new migration for add machine employment date

---
+++ Factory/Migrations/20210604134339_AddMachineInstallationDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class AddMachineInstallationDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "InstallationDate",
+                table: "Machine",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstallationDate",
+                table: "Machine");
+        }
+    }
+}
