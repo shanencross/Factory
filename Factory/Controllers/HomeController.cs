be83ba0efa9ed33979bde139ee6b7db8f8157457
Shanen Cross <shanencross@yahoo.com>
2021-06-04 06:09:13 -0700

    Add skeleton for controllers

---
+++ Factory/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
+
+namespace Factory.Controllers
+{
+  public class HomeController : Controller
+  {
+
+  }
+}
